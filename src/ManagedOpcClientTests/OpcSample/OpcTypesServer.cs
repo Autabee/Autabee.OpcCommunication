47cbf2d5c9baff3d11efce529c0741ebb65b1ea8
I. Arbouw <ian-arbouw-1996@hotmail.com>
2024-08-07 14:59:30 +0200

    Add encoded types with sample opc server

---
+++ src/ManagedOpcClientTests/OpcSample/OpcTypesServer.cs
@@ -0,0 +1,116 @@
+﻿using Autabee.Communication.ManagedOpcClient;
+using Autabee.Communication.ManagedOpcClient.ManagedNode;
+using Autabee.Communication.ManagedOpcClient.Utilities;
+using Autabee.Utility.Logger;
+using Autabee.Utility.Logger.xUnit;
+using AutabeeTestFixtures;
+using Opc.Ua;
+using Quickstarts.DataTypes.Instances;
+using Quickstarts.DataTypes.Types;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Autabee.Communication.OpcCommunicatorTests.OpcSample
+{
+    public class OpcTypeSampleTests : IClassFixture<OpcUaTypeSampleFixture>
+    {
+        private readonly AutabeeManagedOpcClient communicator;
+        private readonly bool skipServerNotFound;
+        private readonly IAutabeeLogger logger;
+
+        public OpcTypeSampleTests(OpcUaTypeSampleFixture testPlcTestsFixture, ITestOutputHelper outputHelper)
+        {
+            communicator = testPlcTestsFixture.Communicator;
+            skipServerNotFound = testPlcTestsFixture.SkipServerNotFound;
+            logger = new AutabeeXunitLogger(outputHelper);
+        }
+
+        [SkippableFact]
+        public void ConnectWithTestServer()
+        {
+            Skip.If(skipServerNotFound, "Server not Found");
+
+            var vehicleInLot = NodeId.Parse("ns=4;i=283");
+
+            //communicator.Session.Factory.AddEncodeableTypes(typeof(ParkingLotType).Assembly);
+
+            var data = communicator.ReadValue(vehicleInLot);
+
+            if (data is object[] nData)
+            {
+                //check if it correctly maps sub types
+                if (nData[0] is Dictionary<string, object> obj)
+                {
+                    Assert.Contains("CargoCapacity", obj);
+                }
+            }
+        }
+
+
+
+        [SkippableFact]
+        public void ConnectWithTestServerDefindedTypes()
+        {
+            Skip.If(skipServerNotFound, "Server not Found");
+
+            var vehicleInLot = NodeId.Parse("ns=4;i=283");
+
+            communicator.AddTypeAssemby(typeof(ParkingLotType).Assembly);
+
+            var data = communicator.ReadValue(vehicleInLot) as object[];
+
+            Assert.IsType<TruckType>(data[0]);
+            Assert.IsType<CarType>(data[1]);
+            Assert.IsType<CarType>(data[2]);
+            Assert.IsType<BicycleType>(data[3]);
+            Assert.IsType<ScooterType>(data[4]);
+            
+        }
+
+        [SkippableFact]
+        public void WriteValue()
+        {
+            Skip.If(skipServerNotFound, "Server not Found");
+
+            var LotType = NodeId.Parse("ns=4;i=380");
+
+            communicator.AddTypeAssemby(typeof(ParkingLotType).Assembly);
+
+            communicator.WriteValue(LotType, (int)2 );
+
+
+            var vehicleInLot = NodeId.Parse("ns=4;i=283");
+            communicator.WriteValue(vehicleInLot, new VehicleType[]
+            {
+                new TruckType
+                {
+                    Engine = EngineType.Diesel,
+                    Make = "DAFF",
+                    Model = "DAFF Transit",
+                    CargoCapacity = 5000
+
+                },
+                 new TruckType
+                {
+                    Engine = EngineType.Diesel,
+                    Make = "DAFF",
+                    Model = "DAFF Transit",
+                    CargoCapacity = 5000
+
+                },
+                  new CarType
+                {
+                    Engine = EngineType.Petrol,
+                    Make = "Chevrolet",
+                    Model = "Kalos",
+                    NoOfPassengers = 2
+
+                }
+            });
+        }
+    }
+}
---
+++ src/TestFixtures/OpcUaTypeSampleFixture.cs
@@ -0,0 +1,48 @@
+﻿using Autabee.Communication.ManagedOpcClient;
+using Opc.Ua;
+using System;
+
+namespace AutabeeTestFixtures
+{
+    public class OpcUaTypeSampleFixture : IDisposable
+    {
+        protected readonly ApplicationDescription server;
+        public bool SkipServerNotFound { get; private set; }
+        public AutabeeManagedOpcClient Communicator { get; private set; }
+
+        //private readonly bool UseAnonymous = false;
+        protected readonly EndpointDescriptionCollection endpoints;
+
+        protected string endpointString = "opc.tcp://localhost:62555/DataTypesServer";
+        protected TestUser plcUser;
+
+        public OpcUaTypeSampleFixture()
+        {
+            InitializeDefaultCommunicator();
+        }
+
+        private void InitializeDefaultCommunicator(bool force = false)
+        {
+            if (Communicator == null || force)
+            {
+                try
+                {
+                    Communicator = new AutabeeManagedOpcClient("Autabee", "UnitX.ApiTester", "Autabee.Tester");
+                    var task = Communicator.Connect(endpointString, userIdentity: new UserIdentity());
+                    task.Wait(10000);
+                    SkipServerNotFound = task.IsCompleted && !Communicator.Connected;
+                }
+                catch (Exception)
+                {
+                    //Logger.Error("Failed to connect", e);
+                    SkipServerNotFound = true;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Communicator?.Disconnect();
+        }
+    }
+}
